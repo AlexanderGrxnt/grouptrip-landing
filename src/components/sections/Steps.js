d262f1c63cc9e37ad39a83a20f467f6a55ab9d83
AlexanderGrxnt <alexandergrxnt@gmail.com>
2020-09-28 09:04:39 +0100

    added steps

--- src/components/sections/Steps.js
+++ src/components/sections/Steps.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
-import Image from '../elements/Image';
+//import Image from '../elements/Image';
 
 const propTypes = {
   ...SectionProps.types
