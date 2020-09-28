f1e8eedbd90e1cd27ab4e9562a6646402b1e11b3
AlexanderGrxnt <alexandergrxnt@gmail.com>
2020-09-28 22:29:53 +0100

    added steps

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -65,23 +65,20 @@ const FeaturesSplit = ({
           <div className={splitClasses}>
 
             <div className="split-item">
-              {/* <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
-                </div>
-                <h3 className="mt-0 mb-12">
-                  Data-driven insights
-                </h3>
-                <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-              </div> */}
+
               <div className={
               classNames(
                 'split-item-image center-content-mobile reveal-from-bottom',
-                imageFill && 'split-item-image-fill'
+                imageFill && 'split-item-image-fill',
               )}
               data-reveal-container=".split-item">
+                <h2 className="mt-0 reveal-from-bottom step-header" data-reveal-delay="200">
+                  <span className="text-color-primary">Step 1.</span>
+                </h2>
+                <h2 className="step-header">
+                  Create a new trip
+                </h2>
+
                 <Image
                   src={require('./../../assets/images/step1.png')}
                   alt="Features split 01"
@@ -91,20 +88,28 @@ const FeaturesSplit = ({
               </div>
 
               <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
-                )}
-                data-reveal-container=".split-item">
+              classNames(
+                'split-item-image center-content-mobile reveal-from-bottom',
+                imageFill && 'split-item-image-fill',
+              )}
+              data-reveal-container=".split-item">
+                <h2 className="mt-0 reveal-from-bottom step-header step-right" data-reveal-delay="200">
+                  <span className="text-color-primary">Step 2.</span>
+                </h2>
+                <h2 className="step-header step-right">
+                  Set Location Options
+                </h2>
+
                 <Image
-                  src={require('./../../assets/images/step2.png')}
-                  alt="Features split 02"
+                  src={require('./../../assets/images/step1.png')}
+                  alt="Features split 01"
                   width={528}
-                  height={396} />
+                  height={396} 
+                />
               </div>
             </div>
 
-            <div className="split-item">
+            {/* <div className="split-item">
                <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   Step 3.
@@ -128,7 +133,7 @@ const FeaturesSplit = ({
                   width={528}
                   height={396} />
               </div>
-            </div>
+            </div> */}
 
             {/* <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
